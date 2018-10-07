266430d398274186d2fccedf147a7b2f513f3219
pnevhuta <pnevhuta@student.wethinkcode.co.za>
2018-10-07 15:40:35 +0200

    auto commit

---
+++ day03/ex01/ex00/ex00/MyViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyViewCell.swift
+//  ex00
+//
+//  Created by Phumudzo NEVHUTALA on 2018/10/05.
+//  Copyright © 2018 Phumudzo NEVHUTALA. All rights reserved.
+//
+
+import UIKit
+
+class MyViewCell: UICollectionViewCell {
+    
+}
