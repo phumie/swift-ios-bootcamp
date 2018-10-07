4929f970dc69b468d4d61bdd2dbdbef7b14ea429
pnevhuta <pnevhuta@student.wethinkcode.co.za>
2018-10-07 15:38:42 +0200

    auto commit

---
+++ day03/ex00/ex00/MyCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCell.swift
+//  ex00
+//
+//  Created by Phumudzo NEVHUTALA on 2018/10/05.
+//  Copyright © 2018 Phumudzo NEVHUTALA. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+}
